9d5ef65fe6933f693dc3e8eb207661b1086e9907
Pragyesh Kumar Seth <spragyesh86@gmail.com>
2024-09-26 15:23:09 +0530

    26/09/2024

--- LinkedList.java
+++ LinkedList.java
@@ -1,33 +1,68 @@
-public class LinkedList {
+class LinkedList {
+    class Node {
+        int data;
+        Node next;
 
-    public static void main(String args[]) {
-
-        Node head = new Node(1);
-        Node temp = head;
-        int data = 2;
-        int n = 50;
-
-        while (n-- > 0) {
-            Node newBlock = new Node();
-            temp.next = newBlock;
-            temp = temp.next;
-
+        Node(int data) {
+            this.data = data;
+            this.next = null;
         }
 
-        System.out.println("Program run Successfully");
     }
 
-}
+    Node head;
 
-class Node {
-    int data;
-    Node next;
-
-    Node() {
+    public LinkedList() {
+        head = null;
     }
 
-    Node(int value) {
-        this.data = value;
-        this.next = null;
+    public Node addNode(int data) {
+        Node temp = new Node(data);
+        temp.next = head;
+        head = temp;
+        return head;
+    }
+
+    public int count() {
+        Node temp = head;
+        int count = 0;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public boolean search(int x){
+        Node temp = head;
+        while(temp != null){
+            if(temp.data == x){
+                return true;
+            }
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList obj = new LinkedList();
+        obj.addNode(0);
+        obj.display();
+        for (int i = 1; i < 100; i += 5) {
+            obj.addNode(i);
+            obj.display();
+        }
+        System.out.println(obj.count());
+        System.out.println(obj.search(16
+        ));
     }
 }
